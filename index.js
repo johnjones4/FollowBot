07c52376a44009ebeac2171257395845712fc5e9
John Jones <johnjones4@gmail.com>
2016-11-13 13:30:54 -0500

    initial

---
+++ index.js
@@ -0,0 +1,189 @@
+const kue = require('kue');
+const request = require('request');
+const querystring = require('querystring');
+const config = require('./config');
+
+const JobName = 'followbot';
+const Delay = 60000;
+const JobTypes = {
+  'SEARCH': 'search',
+  'LISTFRIENDS': 'listFriends',
+  'FOLLOW': 'follow',
+}
+const URLBase = 'https://api.twitter.com/1.1/';
+
+var lastJobTime = 0;
+var users = config.users;
+
+const queue = kue.createQueue();
+kue.app.listen(3000);
+
+for(var userId in users) {
+  users[userId].lastJobTime = 0;
+  queue.create(JobName, {
+    'title': 'Search for ' + userId,
+    'type': JobTypes.SEARCH,
+    'user': userId
+  }).attempts(10000).save();
+}
+
+process.once('SIGTERM',function ( sig ) {
+  queue.shutdown(5000,function(err) {
+    if (err) {
+      console.error(err);
+    }
+    process.exit(0);
+  });
+});
+
+queue.on('error',function(err) {
+  console.error(err);
+});
+
+queue.process(JobName, function(job,done){
+  var domain = require('domain').create();
+  domain.on('error',function(err){
+    done(err);
+  });
+  domain.run(function(){
+    var now = new Date().getTime();
+    if (now - users[job.data.user].lastJobTime > Delay) {
+      processJob(job,done);
+    } else {
+      var backoff = ((Math.random() * (Delay / 2)) + Delay) - (now - users[job.data.user].lastJobTime);
+      console.log('Delaying job by ' + backoff)
+      setTimeout(function() {
+        processJob(job,done);
+      },backoff);
+    }
+  });
+});
+
+function processJob(job,done) {
+  console.log('Processing job ' + job.data.type + ' job for ' + job.data.user);
+  users[job.data.user].lastJobTime = new Date().getTime();
+  switch(job.data.type) {
+    case JobTypes.SEARCH:
+      var params = {
+        'q': users[job.data.user].search,
+        'count': 100
+      };
+      if (job.data.since_id) {
+        params.since_id = job.data.since_id;
+      } else if (job.data.max_id) {
+        params.max_id = job.data.max_id;
+      }
+      request.get({
+        'url': URLBase + 'search/tweets.json?' + querystring.stringify(params),
+        'oauth': users[job.data.user].oauth,
+        'json': true
+      },function(err, r, body) {
+        if (err) {
+          done(err);
+        } else if (body.errors) {
+          done({'error':body.errors});
+        } else {
+          var usersToFollow = body.statuses.map(function(status) {
+            return status.user;
+          }).filter(function(user) {
+            if (!users[job.data.user].following) {
+              users[job.data.user].following = [];
+            }
+            if (users[job.data.user].following.indexOf(user.id) < 0) {
+              users[job.data.user].following.push(user.id);
+              return true;
+            } else {
+              return false;
+            }
+          });
+          usersToFollow.forEach(function(user) {
+            queue.create(JobName, {
+              'title': 'Follow ' + user.screen_name,
+              'type': JobTypes.FOLLOW,
+              'user': job.data.user,
+              'follow': user.id
+            }).attempts(10000).save();
+          });
+          queue.create(JobName, {
+            'title': 'Find friends for ' + job.data.user,
+            'type': JobTypes.LISTFRIENDS,
+            'user': job.data.user
+          }).attempts(10000).save();
+          if (body.search_metadata && body.search_metadata.max_id) {
+            queue.create(JobName, {
+              'title': 'Search for ' + job.data.user + ' (Previous ' + body.search_metadata.max_id + ')',
+              'type': JobTypes.SEARCH,
+              'user': job.data.user,
+              'max_id': body.search_metadata.max_id
+            }).attempts(10000).save();
+          }
+          if (body.search_metadata && body.search_metadata.since_id) {
+            queue.create(JobName, {
+              'title': 'Search for ' + job.data.user + ' (Next ' + body.search_metadata.since_id + ')',
+              'type': JobTypes.SEARCH,
+              'user': job.data.user,
+              'since_id': body.search_metadata.since_id
+            }).attempts(10000).save();
+          }
+          done();
+        }
+      });
+      break;
+    case JobTypes.LISTFRIENDS:
+      var params = {
+        'user_id': job.data.user,
+        'count': 5000
+      };
+      if (job.data.cursor) {
+        params.cursor = job.data.cursor;
+      }
+      request.get({
+        'url': URLBase + 'friends/ids.json?' + querystring.stringify(params),
+        'oauth': users[job.data.user].oauth,
+        'json': true
+      },function(err, r, body) {
+        if (err) {
+          done(err);
+        } else if (body.errors) {
+          done({'error':body.errors});
+        } else {
+          if (!users[job.data.user].following) {
+            users[job.data.user].following = [];
+          }
+          body.ids.forEach(function(id) {
+            if (users[job.data.user].following.indexOf(id) < 0) {
+              users[job.data.user].following.push(id);
+            }
+          });
+          if (body.next_cursor && body.next_cursor > 0) {
+            queue.create(JobName, {
+              'title': 'Find friends for ' + job.data.user + ' (' + body.next_cursor + ')',
+              'type': JobTypes.LISTFRIENDS,
+              'user': job.data.user,
+              'cursor': body.next_cursor
+            }).attempts(10000).save();
+          }
+          done();
+        }
+      });
+      break;
+    case JobTypes.FOLLOW:
+      done();
+      request.post({
+        'url': URLBase + 'friendships/create.json?' + querystring.stringify({'user_id':job.data.follow,'follow':true}),
+        'oauth': users[job.data.user].oauth,
+        'json': true
+      },function(err, r, body) {
+        if (err) {
+          done(err);
+        } else {
+          if (body.errors) {
+            done({'error':body.errors});
+          } else {
+            done();
+          }
+        }
+      });
+      break;
+  }
+}
